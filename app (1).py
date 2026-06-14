492aba69bacc5774779424496098368a4fc57555
Farhan Khan <yasirkhan443296@gmail.com>
2026-06-14 23:23:17 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,17 @@
+
+import streamlit as st
+import pickle
+
+st.title("Loan Approval Prediction")
+
+try:
+    model = pickle.load(open("model.pkl", "rb"))
+        columns = pickle.load(open("columns.pkl", "rb"))
+
+            st.success("Model Loaded Successfully")
+                st.write("Number of features:", len(columns))
+                    st.write(columns)
+
+                    except Exception as e:
+                        st.error(e)
+                        
