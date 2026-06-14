70e91332a5b9eae144359dd2da3494ba0e11ac2b
Farhan Khan <yasirkhan443296@gmail.com>
2026-06-14 23:33:35 +0530

    Fix indentation in exception handling

--- app (1).py
+++ app (1).py
@@ -12,6 +12,6 @@ try:
     st.write("Number of features:", len(columns))
     st.write(columns)
 
- except Exception as e:
-                       st.error(e)
+except Exception as e:
+    st.error(e)
                         
